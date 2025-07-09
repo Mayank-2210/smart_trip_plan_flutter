aa0498081ffaeab61b585f996da409fca00607a5
Mayank-2210 <mayank019283pandey@gmail.com>
2025-07-10 00:13:24 +0530

    Update app title

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Smart Trip Plan by Mayank',
       theme: ThemeData(
         // This is the theme of your application.
         //
